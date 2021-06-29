e65d12298dcb9710330339ee21756338b32e6c49
h1m <himanshu.yadav643@gmail.com>
2021-06-29 10:59:27 +0530

    added controller and model packages

--- src/main/java/technicalblog/controller/HomeController.java
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -1,2 +1,13 @@
-package technicalblog.controller;public class HomeController {
+package technicalblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String getAllPosts() {
+
+    }
 }
--- src/main/java/technicalblog/model/Post.java
+++ src/main/java/technicalblog/model/Post.java
@@ -1,2 +1,36 @@
-package technicalblog.model;public class Post {
+package technicalblog.model;
+
+import java.util.Date;
+
+public class Post {
+
+    // characteristics of a blog post
+    private String title;
+    private String body;
+    private Date date;
+
+    // getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
