3fc0c99539b3537bb2149a86201f3ad178220f49
h1m <himanshu.yadav643@gmail.com>
2021-06-30 11:41:52 +0530

    working on header and footer of index.html

--- src/main/java/technicalblog/controller/HomeController.java
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -1,20 +1,26 @@
 package technicalblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import technicalblog.model.Post;
+import technicalblog.service.PostService;
 
 import java.util.ArrayList;
-import java.util.Date;
+
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private PostService postService;
+
     @RequestMapping("/")
     public String getAllPosts(Model model) {
 
-
+        // call to PostService class using spring ioc
+        ArrayList<Post> posts = postService.getAllPosts();
 
         // preparing the model object
         model.addAttribute("posts", posts);
