033d4e4d449ff87c213f321769e06d98232bf0dc
h1m <himanshu.yadav643@gmail.com>
2021-06-30 10:43:02 +0530

    added the PostService class to create the posts

--- src/main/java/technicalblog/controller/HomeController.java
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -14,22 +14,7 @@ public class HomeController {
     @RequestMapping("/")
     public String getAllPosts(Model model) {
 
-        ArrayList<Post> posts = new ArrayList<>();
 
-        // creating an object of class Post
-        Post post1 = new Post();
-        post1.setTitle("Post 1");
-        post1.setBody("Post 1 Body");
-        post1.setDate(new Date());
-
-        Post post2 = new Post();
-        post2.setTitle("Post 2");
-        post2.setBody("Post 2 Body");
-        post2.setDate(new Date());
-
-        // adding the posts in the arraylist
-        posts.add(post1);
-        posts.add(post2);
 
         // preparing the model object
         model.addAttribute("posts", posts);
---
+++ src/main/java/technicalblog/service/PostService.java
@@ -0,0 +1,35 @@
+package technicalblog.service;
+
+import org.springframework.stereotype.Service;
+import technicalblog.model.Post;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@Service
+public class PostService {
+
+    // we are building the posts here
+    public ArrayList<Post> getAllPosts() {
+
+        ArrayList<Post> posts = new ArrayList<>();
+
+        // creating an object of class Post
+        Post post1 = new Post();
+        post1.setTitle("Post 1");
+        post1.setBody("Post 1 Body");
+        post1.setDate(new Date());
+
+        Post post2 = new Post();
+        post2.setTitle("Post 2");
+        post2.setBody("Post 2 Body");
+        post2.setDate(new Date());
+
+        // adding the posts in the arraylist
+        posts.add(post1);
+        posts.add(post2);
+
+        return posts;
+
+    }
+}
