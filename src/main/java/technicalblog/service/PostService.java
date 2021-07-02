2eed4ef942a280b5b2db0bbb993a958820e4e2d7
h1m <himanshu.yadav643@gmail.com>
2021-07-02 11:10:14 +0530

    working with JDBC to connect the application with Postgres

--- src/main/java/technicalblog/service/PostService.java
+++ src/main/java/technicalblog/service/PostService.java
@@ -3,6 +3,7 @@ package technicalblog.service;
 import org.springframework.stereotype.Service;
 import technicalblog.model.Post;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -14,20 +15,42 @@ public class PostService {
 
         ArrayList<Post> posts = new ArrayList<>();
 
-        // creating an object of class Post
-        Post post1 = new Post();
-        post1.setTitle("Post 1");
-        post1.setBody("Post 1 Body");
-        post1.setDate(new Date());
-
-        Post post2 = new Post();
-        post2.setTitle("Post 2");
-        post2.setBody("Post 2 Body");
-        post2.setDate(new Date());
-
-        // adding the posts in the arraylist
-        posts.add(post1);
-        posts.add(post2);
+//        // creating an object of class Post
+//        Post post1 = new Post();
+//        post1.setTitle("Post 1");
+//        post1.setBody("Post 1 Body");
+//        post1.setDate(new Date());
+//
+//        Post post2 = new Post();
+//        post2.setTitle("Post 2");
+//        post2.setBody("Post 2 Body");
+//        post2.setDate(new Date());
+//
+//        // adding the posts in the arraylist
+//        posts.add(post1);
+//        posts.add(post2);
+        
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalBlog", "postgres", "himanshu");
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM posts");
+            while(rs.next()) {
+                Post post = new Post();
+                post.setTitle(rs.getString("title"));
+                post.setBody(rs.getString("body"));
+                posts.add(post);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
 
         return posts;
     }
