e278146ddc4223240f9e55c1e3c16afcf3736fca
h1m <himanshu.yadav643@gmail.com>
2021-07-03 13:43:24 +0530

    converting Post class to entity

--- src/main/java/technicalblog/model/Post.java
+++ src/main/java/technicalblog/model/Post.java
@@ -1,12 +1,26 @@
 package technicalblog.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
+@Entity
+@Table(name="posts")
 public class Post {
 
     // characteristics of a blog post
+    @Id
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="title")
     private String title;
+
+    @Column(name="body")
     private String body;
+
     private Date date;
 
     // getters and setters
